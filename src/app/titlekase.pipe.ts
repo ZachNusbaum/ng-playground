28c5c7c6e73ecc759480cad3421b18938668392e
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-07-18 09:20:41 -0700

    Finish implementing new titlekase pipe

--- src/app/titlekase.pipe.ts
+++ src/app/titlekase.pipe.ts
@@ -5,8 +5,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TitlekasePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: string, args?: any): any {
+    if (!value) { return null; }
+
+    let prepositions = [
+      'of',
+      'the'
+    ];
+
+    let words = value.split(' ');
+    for (let i = 0; i < words.length; i++) {
+      if (i > 0 && prepositions.includes(words[i])) {
+        words[i] = words[i].toLowerCase();
+      } else {
+        words[i] = words[i].substr(0,1).toUpperCase() + words[i].substr(1).toLowerCase();
+      }
+    }
+
+    return words.join(' ');
   }
 
 }
