d6acdfc7c974c339a6c338f26c63ce361d601bb1
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-07-18 08:27:54 -0700

    Import the FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { AuthorsService } from './authors.service';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -11,7 +12,8 @@ import { AuthorsComponent } from './authors/authors.component';
     AuthorsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [AuthorsService],
   bootstrap: [AppComponent]
