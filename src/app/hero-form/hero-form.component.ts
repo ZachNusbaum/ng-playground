a80f4f36df35ef24015f331a69b67bd9024905a3
Zach Nusbaum <zachdnusbaum@gmail.com>
2018-07-18 13:16:54 -0700

    Set up the logic for the HeroForm component

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from './../hero';
 
 @Component({
   selector: 'app-hero-form',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-form.component.css']
 })
 export class HeroFormComponent implements OnInit {
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
 
-  constructor() { }
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  get diagnostic() { return JSON.stringify(this.model); }
+
+  // constructor() { }
 
   ngOnInit() {
   }
